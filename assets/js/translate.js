1ebef6ea0b3214970a41034a031e69668670eac1
Randy-420 <randy.skinner@hotmail.com>
2021-12-23 12:01:36 -0500

    .

--- assets/js/translate.js
+++
@@ -1,7 +0,0 @@
-            function googleTranslateElementInit() {
-                new google.translate.TranslateElement({
-                    multilanguagePage: true,
-                    gaTrack: true
-                }, 'google_translate_element');
-            }
-
