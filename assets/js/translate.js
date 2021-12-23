bdab1269bf7c09d4f45ccb2874ef8e257f567781
Randy-420 <randy.skinner@hotmail.com>
2021-12-23 12:00:57 -0500

    .

---
+++ assets/js/translate.js
@@ -0,0 +1,7 @@
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({
+                    multilanguagePage: true,
+                    gaTrack: true
+                }, 'google_translate_element');
+            }
+
